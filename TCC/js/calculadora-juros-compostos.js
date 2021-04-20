2fe14f36e8b095465695558a38ba255ed319392c
Diego Ramos Brito <diegorbrito9@gmail.com>
2021-04-20 20:24:43 -0300

    criando o js da calculadora de juros

--- TCC/js/calculadora-juros-compostos.js
+++ TCC/js/calculadora-juros-compostos.js
@@ -1,9 +1,43 @@
 var botao = document.querySelector("#calcular");
 
+//Adicionando um escutador de eventos, para manipular o formulario 
 botao.addEventListener("click",calcular)
 
 function calcular(event){
 
+    // Desativando o comportamento padrão do botão, para não atualizar a pagina
     event.preventDefault();
+
+    //Recebendo a tebala, para pegar os valores dos inputs
+    let form = document.querySelector("#info-calculo");
+
+    // Pegando o valor dos inputs de "aporte inicial", "aporte mensal", "juros", "tempo"
+    let aporteInicial = form.aporteInicial.value;
+    let aporteMensal = form.aporteMensal.value;
+    let juros = form.juros.value;
+    let tempo = form.tempo.value
+
+
+
+    // Pegando as "li" do campo de "% de juros"
+    let liJurosAnual = document.querySelector("#juros-anual");
+    let liJurosMensal = document.querySelector("#juros-mensal");
+
+    //Pegando as "li" do campo de "tempo"
+    let liTempoMes = document.querySelector("#tempoMeses");
+    let liTempoAno = document.querySelector("#tempoAnual")
+
+    
+    
+    // Recebendo o conteudo das li
+    let tempoMes = liTempoMes.textContent;
+    let tempoAno = liTempoAno.textContent;
+
+    // Recebendo os conteudos das "li"
+    let jurosAnual = liJurosAnual.textContent;
+    let jurosMensal = liJurosMensal.textContent;
+
+    
+    
     
 }
